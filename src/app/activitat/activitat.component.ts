70722a5c337f197b7131aa5e03073538186eb5b8
diazdani <daniel.diaz@institutvidreres.cat>
2023-11-20 11:26:46 +0100

    initial comit

---
+++ src/app/activitat/activitat.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-activitat',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './activitat.component.html',
+  styleUrl: './activitat.component.css'
+})
+export class ActivitatComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ActivitatComponent} from "./activitat/activitat.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+    imports: [CommonModule, RouterOutlet, ActivitatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
