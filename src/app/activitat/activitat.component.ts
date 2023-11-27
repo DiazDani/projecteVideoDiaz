a2ca5278c3dd1a9b3990de273fc0adc94768f6c3
diazdani <daniel.diaz@institutvidreres.cat>
2023-11-27 10:46:51 +0100

    commit base pagina

--- src/app/activitat/activitat.component.ts
+++ src/app/activitat/activitat.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-activitat',
@@ -9,5 +10,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './activitat.component.css'
 })
 export class ActivitatComponent {
+  private socket = io('http://172.30.240.1:8888', { transports : ['websocket'] });
 
+  codigoGenerado: string|undefined;
+
+  constructor() {
+    this.socket.on('hello', (args) => {
+      console.log(args);
+    });
+  }
+
+  generarCodigo() {
+    this.socket.emit('generarCodigo', (args: any)=>{
+      this.codigoGenerado=args
+      console.log(args)
+    } );
+  }
 }
